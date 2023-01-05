cba5c47552e252414440d7a0b4016d33780d7ce0
ryeongse25 <seryeong971120@gmail.com>
2023-01-05 21:30:45 +0900

    study: form validation check

--- NextJS/NomadCoders_NextJS/pages/form.js
+++ NextJS/NomadCoders_NextJS/pages/form.js
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Seo from "../components/Seo";
 import { Form } from "react-bootstrap";
 
 export default function form() {
+  const form = useRef(null);
+
   const [formValue, setFormValue] = useState({
     name: "",
     email: "",
@@ -17,51 +19,56 @@ export default function form() {
   };
 
   const sendEmail = async () => {
-    const response = await fetch(
+    const formData = new FormData();
+
+    formData.append("name", formValue.name);
+    formData.append("email", formValue.email);
+    formData.append("message", formValue.message);
+
+    let result = form.current.checkValidity();
+
+    if (!result) {
+      form.current.reportValidity();
+      return false;
+    }
+
+    await fetch(
       "https://script.google.com/macros/s/AKfycbzB54BUV1QBdArkKEIJhPiJz4aCTP7sjq6cLKXPqQad9uTJXAbNPS2dcOhpnbAZ17-B/exec",
       {
         method: "POST",
         mode: "no-cors",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          name: formValue.name,
-          email: formValue.email,
-          message: formValue.message,
-        }),
+        body: formData,
       }
     );
+    console.log("sent");
   };
   return (
     <section>
       <Seo title="Form" />
       <h2>Email Me!</h2>
-      <Form
-        className="gform"
-        action="https://script.google.com/macros/s/AKfycbzB54BUV1QBdArkKEIJhPiJz4aCTP7sjq6cLKXPqQad9uTJXAbNPS2dcOhpnbAZ17-B/exec"
-        method="POST"
-        target="iframe1"
-      >
+      <Form ref={form}>
         <Form.Control
           name="name"
           placeholder="name"
           onChange={onChange}
+          required
         ></Form.Control>
         <Form.Control
           name="email"
           placeholder="email"
           onChange={onChange}
+          required
         ></Form.Control>
         <Form.Control
           name="message"
           placeholder="message"
           onChange={onChange}
+          required
         ></Form.Control>
-        <button type="submit">Send Message</button>
-        <div style={{ display: "none" }} className="thankyou_message">
-          <h2>Thanks for contacting us! We will get back to you soon!</h2>
-        </div>
+        <button type="button" onClick={sendEmail}>
+          Send Message
+        </button>
       </Form>
-      <iframe id="iframe1" name="iframe1" style={{ display: "none" }}></iframe>
     </section>
   );
 }
